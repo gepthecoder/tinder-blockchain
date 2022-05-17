a56a044b1903df13a6cbcfa5b12ad1666f5d96e7
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-17 22:47:17 +0200

    $0011: [Header]: create header component

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div className={style.wrapper}>
       {/*<Header />*/}
       <div className={style.cardsContainer}>
-        {/*<Header />*/}
+        {/*<Card />*/}
       </div>
     </div>
   )
