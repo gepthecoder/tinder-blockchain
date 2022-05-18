572e47eb9d5543cbbeff2d587c4e9e3c519a07e3
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-18 10:26:33 +0200

    $0019: [Content] [SwipeCards]: create card header component with profile img, logo and notifications

--- components/Card.js
+++ components/Card.js
@@ -1,3 +1,5 @@
+import { SiTinder } from 'react-icons/si'
+import CardHeader from './CardHeader'
 
 const style = {
   wrapper: `h-[45rem] w-[27rem] flex flex-col rounded-lg overflow-hidden`,
@@ -12,9 +14,21 @@ const Card = () => {
 
   return (
     <div className={style.wrapper}>
+        <CardHeader />
         <div className={style.cardMain}>
-            
+            <div className={style.noMoreWrapper}>
+                <SiTinder className={style.tinderLogo} />
+                <div className={style.noMoreText}>
+                    No More Profiles in your Location...
+                </div>
+            </div>
+            <div className={style.swipesContainer}>
+                {/* {cardsData.map((card, index) => (
+                    <TinderCardItem card={card} key={index} />
+                ))} */}
+            </div>
         </div>
+        {/* <CardFooter /> */}
     </div>
   )
 }
