790c4284aaa9c6fbfe157528dfaf60a748679fe0
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-18 21:32:57 +0200

    $0054: [Header]: show current user profile picture

--- components/CardHeader.js
+++ components/CardHeader.js
@@ -3,6 +3,8 @@ import profilePic from '../assets/profile-pic.jpg'
 import logo from '../assets/logo.png'
 import { IoIosNotifications } from 'react-icons/io'
 import Image from 'next/image'
+import { useContext } from 'react'
+import { TinderContext } from '../context/TinderContext'
 
 const style = {
   wrapper: `flex items-center bg-white w-full h-20 p-8 justify-evenly`,
@@ -13,10 +15,13 @@ const style = {
 }
 
 const CardHeader = () => {
+
+  const { currentUser } = useContext(TinderContext)
+
   return (
     <div className={style.wrapper}>
       <Image
-        src={profilePic}
+        src={currentUser?.imageUrl || profilePic}
         width={40}
         height={40}
         alt='profile-pic'
