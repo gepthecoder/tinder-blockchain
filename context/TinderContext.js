1cb19b23b87c9b0c4439b661fee02a6a86af9d82
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-18 11:59:38 +0200

    $0027: [Backend] [Moralis] [Context]: check for wallet connection

--- context/TinderContext.js
+++ context/TinderContext.js
@@ -1,15 +1,29 @@
 import { useState, createContext, useEffect } from 'react'
 import { faker } from '@faker-js/faker'
+import { useMoralis } from 'react-moralis'
 
 export const TinderContext = createContext()
 
 export const TinderProvider = ({children}) => {
    
-    [/* each user that we are able to swipe through labeled as a card  */]
+    /* each user that we are able to swipe through labeled as a card  */
     const [cardsData, setCardsData] = useState([])
     const [currentAccount, setCurrentAccount] = useState()
     const [currentUser, setCurrentUser] = useState()
 
+    const { authenticate, isAuthenticated, user, Moralis } = useMoralis()
+
+    /* Moralis knows in the background if we are connected or not .. :o */
+    const checkWalletConnection = async () => {
+        if (isAuthenticated) {
+          const address = user.get('ethAddress')
+          setCurrentAccount(address)
+          requestToCreateUserProfile(address, faker.name.findName())
+        } else {
+        /* There shouldn't be an account if the user is not created */
+          setCurrentAccount('')
+        }
+      }
 
     return (
         <TinderContext.Provider
