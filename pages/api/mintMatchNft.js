3c018826cb6a1cde132b3986edec08df038786bc
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-18 19:12:27 +0200

    $0051: [TinderMatch] [MintingNFTs]: invoke tinder contract and mint nft for bot participants - publish to opeansea 🖼🎨

--- pages/api/mintMatchNft.js
+++ pages/api/mintMatchNft.js
@@ -18,6 +18,52 @@ const mintMatchNft = async (req, res) => {
         } just matched <3`,
         image: `ipfs://QmY4tKpDGzVHzaSkQc5gzVMCMNoznZqaX15DXkyL2bPp8Z`,
     }
+
+    /* Abbreviation from binary to ASCI:
+        -> takes meta data 
+        -> to string 
+        -> makes buffer for it 
+        -> encode to base64 
+    */
+    const toBtoa = Buffer.from(JSON.stringify(metadata)).toString('base64')
+    const metadataFile = new Moralis.File('file.json', { base64: toBtoa })
+
+    await metadataFile.saveIPFS({ useMasterKey: true })
+
+    const metadataURI = metadataFile.ipfs()
+
+    const provider = ethers.getDefaultProvider(process.env.ALCHEMY_API_URL, {
+        chainId: 4,
+        name: 'rinkeby',
+    })
+
+    /*  GASLESS TRANSACTIONS:
+        -> anytime the is gas fee its gonna be paid for by the owner of the smart contract address (private key)
+    */
+    const walletWithProvider = new ethers.Wallet(
+        process.env.WALLET_PRIVATE_KEY,
+        provider,
+    )
+    
+    const contract = new ethers.Contract(
+        TINDER_ADDRESS,
+        TINDER_ABI,
+        walletWithProvider,
+    )
+
+    /* Invoke tinder contract with mint nft function [TinderERC721] */
+    const tx = await contract.mintNFT(
+        req.body.walletAddresses[0],
+        req.body.walletAddresses[1],
+        metadataURI,
+    )
+
+    const txReceipt = await tx.wait()
+
+    res.status(200).send({
+        message: 'success',
+        data: { tx: tx, txReceipt: txReceipt },
+      })
 }
   
 export default mintMatchNft
